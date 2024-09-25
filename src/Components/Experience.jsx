9cd94db8287d40eb335d66d983f86b92ae41b1ba
farhanreyy <reypialfarizi@gmail.com>
2024-09-26 02:08:42 +0700

    fixed experience title

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -18,7 +18,7 @@ const Experience = () => {
       <div className="container mx-auto px-4 md:px-16 lg:px-24">
         <h2
           data-aos="fade-down"
-          className="text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold mb-10 md:mb-20 leading-normal uppercase bg-gradient-to-r text-transparent bg-clip-text from-white to bg-purple-600"
+          className="text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold mb-10 md:mb-20 leading-normal uppercase bg-gradient-to-r text-transparent bg-clip-text from-white to bg-purple-600 break-words"
         >
           Experience
         </h2>
