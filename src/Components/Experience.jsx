e3e3740cc76201b70a6c5af5a4debf8b92f05960
farhanreyy <reypialfarizi@gmail.com>
2024-10-15 09:02:24 +0700

    changed text

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const TextExperience = ({ date, company, details }) => {
   return (
     <div className="md:flex gap-x-6 text-white font-bold text-lg md:text-lg lg:text-xl mb-6">
-      {/* Bagian tanggal */}
       <div className="flex font-normal md:w-1/4 md:justify-end mb-2 md:mb-0">{date}</div>
-      {/* Bagian detail */}
       <div className="md:w-3/4">
         <p className="mb-2 lg:mb-4 text-white font-semibold">{company}</p>
         <p className="font-normal text-sm md:text-base lg:text-lg text-justify leading-6 lg:leading-8">{details}</p>
